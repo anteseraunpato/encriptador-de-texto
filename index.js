4bc924e6c51b894d33862a346b9bfe1ebb032d4e
Jahir <129269093+anteseraunpato@users.noreply.github.com>
2024-03-11 21:46:51 -0600

    Crea el index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+function encriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let mensajeimg = document.getElementById("mensajeimg");
+
+  let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+
+  if (texto.length != 0) {
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto encriptado con éxito";
+    parrafo.textContent = "sólo un genio podrá descifrar tu mensaje";
+    mensajeimg.src = "./img/encriptado.png";
+  } else {
+    mensajeimg.src = "./img/unpato.png";
+    tituloMensaje.textContent = "No hay mensajes";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    swal("¡Alto rufián!", "No ingresaste ningún texto", "warning");
+  }
+}
+
+function desencriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let mensajeimg = document.getElementById("mensajeimg");
+
+  let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u")
+  
+    if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Tu texto fue desencriptado";
+      parrafo.textContent = "¡descubriste el mensaje secreto!";
+      mensajeimg.src = "./img/desencriptado.png";
+    } else {
+      mensajeimg.src = "./img/unpato.png";
+      tituloMensaje.textContent = "No hay mensajes";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      swal("¡Alto rufián!", "No ingresaste ningún texto", "warning");
+    }
+}
