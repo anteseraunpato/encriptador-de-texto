1d1947da24a1e84cc67c789a0d12cc2c7b42dc5a
Jahir <129269093+anteseraunpato@users.noreply.github.com>
2024-03-16 00:02:24 -0600

    Actualización del index.js
    Se arregló el bug que desencriptaba jaimenters como "ja" en lugar de james

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ function desencriptar() {
   let mensajeimg = document.getElementById("mensajeimg");
 
   let textoCifrado = texto
+    .replace(/ai/gi, "a")
     .replace(/enter/gi, "e")
     .replace(/imes/gi, "i")
-    .replace(/ai/gi, "a")
     .replace(/ober/gi, "o")
     .replace(/ufat/gi, "u")
   
